6e5b6cf1ef5d3741392db08d2a32f1dbf26655b3
Himanshu Kumar😊 <130290398+HimanshuKumar501@users.noreply.github.com>
2025-04-19 14:24:22 +0530

    Add files via upload

---
+++ CSS/Script.js
@@ -0,0 +1,76 @@
+// Animaton One Start & Stop Button
+function Start_one()
+{
+      document.getElementById("id_fourty_two").style.animationPlayState="running";
+}
+
+function Stop_one()
+{
+      document.getElementById("id_fourty_two").style.animationPlayState="paused";
+}
+
+// Animaton Two Start & Stop Button
+function Start_two()
+{
+      document.getElementById("id_fourty_three").style.animationPlayState="running";
+}
+
+function Stop_two()
+{
+      document.getElementById("id_fourty_three").style.animationPlayState="paused";
+}
+// Animaton Three Start & Stop Button
+function Start_three()
+{
+      document.getElementById("id_fourty_four").style.animationPlayState="running";
+}
+
+function Stop_three()
+{
+      document.getElementById("id_fourty_four").style.animationPlayState="paused";
+}
+// Animaton Four Start & Stop Button
+function Start_four()
+{
+      document.getElementById("id_fourty_five").style.animationPlayState="running";
+}
+
+function Stop_four()
+{
+      document.getElementById("id_fourty_five").style.animationPlayState="paused";
+}
+
+// Animaton Five Start & Stop Button
+function Start_five()
+{
+      document.getElementById("id_fourty_six").style.animationPlayState="running";
+}
+
+function Stop_five()
+{
+      document.getElementById("id_fourty_six").style.animationPlayState="paused";
+}
+
+// Animaton Six Start & Stop Button
+function Start_six()
+{
+      document.getElementById("id_fourty_seven").style.animationPlayState="running";
+}
+
+function Stop_six()
+{
+      document.getElementById("id_fourty_seven").style.animationPlayState="paused";
+}
+
+
+// Animaton Seven Start & Stop Button
+function Start_seven()
+{
+      document.getElementById("id_fourty_eight").style.animationPlayState="running";
+}
+
+function Stop_seven()
+{
+      document.getElementById("id_fourty_eight").style.animationPlayState="paused";
+}
+
